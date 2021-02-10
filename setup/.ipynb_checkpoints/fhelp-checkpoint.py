7911a478ac9bdb2abbb709e2b4a077dd152d8fdc
Ian Verbeck <ianverbeck@gmail.com>
2021-02-10 14:24:25 -0700

    Finished general company valuations
    Added text file for notes, finished most company valuation tables.

--- setup/.ipynb_checkpoints/fhelp-checkpoint.py
+++ setup/.ipynb_checkpoints/fhelp-checkpoint.py
@@ -3,12 +3,15 @@ import numpy as np
 from urllib.request import urlopen  
 import json
 
-def get_appended(url,tickers,symbol_seperate=False):
+def get_appended(url,tickers,symbol_seperate=False,verbose=False):
     '''Return dataframe with tickers' data appended''' 
     url_temp = url
     tempdf = pd.DataFrame()
-    #iterate through tickers
+    #iterate through ticker
     for ticker in tickers:
+        if verbose:
+            print('Downloading for ticker '+ticker+'....')
+        
         #create valid URL
         final_url = url_temp.replace('***TICKER***',ticker)
         try:
@@ -19,7 +22,7 @@ def get_appended(url,tickers,symbol_seperate=False):
             else:
                 tempdf = tempdf.append(pd.read_json(final_url))
         except Exception as e:
-            print(e,'for ticker'+ticker,', continuing.....')
+            print(e,'for ticker '+ticker,', continuing.....')
             
     return tempdf
         
